d3a0ff3a28034f9451b2778abc57828c4488c288
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-11-22 20:47:44 +0300

    ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("ProjectSmeshariki");
+        database.dropDatabase()
+        database = client.db("ProjectSmeshariki");
+        const Smeshariki= database.collection("Smeshariki");
+        const result = await Smeshariki.insertOne({ name: "Нюша" });
+        console.log(`${result} documents were inserted`);
+    } finally {
+        await client.close();
+    }
+}
+run()
