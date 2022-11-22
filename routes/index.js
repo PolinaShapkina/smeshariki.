0b194d19fe738d88b16b972ea276ef965129a96a
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-11-22 10:15:26 +0300

    СОЗДАНИЕ ШАБЛОНА

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* Страница Нюши*/
+/* Страница Нюши */
 router.get('/Nusha', function(req, res, next) {
-  res.send("<h1>Страница Нюши</h1>")
+  res.render('smeshariki', {
+      title: "Нюша",
+      picture: "images/nyusha.jpg",
+      desc: "Модная свинка"
+  });
 });
+
 
 /* Страница Кроша*/
 router.get('/Krosh', function(req, res, next) {
-  res.send("<h1>Страница Кроша</h1>")
-});
+  res.render('smeshariki', {
+      title: "Крош",
+      picture: "images/krosh.jpg",
+      desc: "Носит красный бант и красное платье."
+    });
+  });
 
-/* Страница Пина */
-router.get('/Pin', function(req, res, next) {
-  res.send("<h1>Страница Пина</h1>")
+
+/* Страница Пина*/
+router.get('/pin', function(req, res, next) {
+  res.render('smeshariki', {
+      title: "Пин",
+      picture: "images/pin.jpg",
+      desc: "Cамый младший член семьи."
+  });
 });
 
 
