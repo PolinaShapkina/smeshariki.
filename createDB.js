34ba84ae29ed6648439fa6e06ebf346ddc0d211b
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-11-23 15:21:06 +0300

    СОЗДАНИЕ МОДУЛЯ ДАННЫХ

--- createDB.js
+++ createDB.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
 
-const uri = "mongodb://localhost:27017/"
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
     try {
         await client.connect();
-        var database = client.db("ProjectSmeshariki");
+        var database = client.db("smeshariki");
         database.dropDatabase()
-        database = client.db("ProjectSmeshariki");
-        const Smeshariki= database.collection("Smeshariki");
+        database = client.db("smeshariki");
+        const Smeshariki= database.collection("fauna");
         const result = await Smeshariki.insertOne({ name: "Нюша" });
         console.log(`${result} documents were inserted`);
     } finally {
