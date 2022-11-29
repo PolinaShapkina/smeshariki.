9e49715255b9c637747682f3dc024c4fd85dad8a
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-11-29 14:29:31 +0300

    ИЗВЛЕЧЕНИЕ ДАННЫХ ИЗ БАЗЫ

--- routes/smeshariki.js
+++ routes/smeshariki.js
@@ -3,13 +3,26 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('Новый маршрутизатор, для маpшрутов, начинающихся с avocados');
+    res.send('Новый маршрутизатор, для маpшрутов, начинающихся с smeshariki');
 });
 
 /* Страница смешариков */
 router.get("/:nick", function (req, res, next) {
     res.send(req.params.nick);
 });
+/* Страница героев */
+router.get('/:nick', function(req, res, next) {
+    smeshariki.findOne({nick:req.params.nick}, function(err,smeshariki){
+        if(err) return next(err)
+        if(!smeshariki) return next(new Error("Нет такого героя в этом мультике"))
+        res.render('smeshariki', {
+            title: smeshariki.title,
+            picture: smeshariki.avatar,
+            desc: smeshariki.desc
+        })
+    })
+})
+
 
 
 module.exports = router;
