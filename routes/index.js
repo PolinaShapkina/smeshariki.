6ab46b5ad240d474bc6776d1b9807920b352497c
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-12-03 22:35:12 +0300

    КОМАНДА ЗАПИСИ В COOKIE

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var smeshariki = require("../models/Smeshariki").smeshariki
 /* GET home page. */
 router.get('/', function(req, res, next) {
     smeshariki.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
+      res.cookie('greeting', 'Hi!!!').render('index',{
                                 title: 'Express',
                                 menu: menu
                             });
