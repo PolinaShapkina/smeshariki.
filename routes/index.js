beb0cc4e0fea7219b90d9e0439ae8227dbae5d03
PolinaShapkina <shapkina.p.1.21@gmail.com>
2022-12-04 22:30:17 +0300

    ФУНКЦИОНАЛЬНОСТЬ LOGOUT

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,14 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход', error:null});
 });
 
+/* POST logout. */
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  res.locals.user = null
+  res.redirect('/')
+});
+
+
 /* POST login/registration page. */
 router.post('/logreg', function (req, res, next) {
   var username = req.body.username
